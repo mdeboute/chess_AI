d12084a865074809e46442c4a841e9cbc5897a55
mdeboute <martin.deboute@gmail.com>
2021-11-17 00:46:18 +0100

    tout fonctionne

--- src/test.py
+++ src/test.py
@@ -3,7 +3,8 @@ import chess
 from random import randint, choice
 
 
-board = chess.Board()
+board = chess.Board("r1bqkbnr/pppp1ppp/8/8/3nP3/8/PPP2PPP/RNBQKB1R w KQkq - 0 5")
+board_2 = chess.Board("r1bqkb1r/ppppnpQp/8/8/4P3/8/PPP2PPP/RNB1KB1R b KQkq - 0 6")
 
 # this function makes an exhaustive search of all the chess games by limiting the depth of
 # the search by a search parameter with the chess library and return the number of possibilities
@@ -68,15 +69,23 @@ def shannonWeight(list):
 
 def evaluator(board=board):
     black, white = getPiece(board)
-    return (shannonWeight(white)-abs(shannonWeight(black)))
+    if(board.turn == chess.WHITE):
+        return shannonWeight(white) - abs(shannonWeight(black))
+    if(board.turn == chess.BLACK):
+        return abs(shannonWeight(black)) - shannonWeight(white)
 
 def getBetterMove(board=board):
     costs = dict()
     moves = board.generate_legal_moves()
     for m in moves:
         board.push(m)
-        costs[m] = evaluator(board)
+        if(board.turn == chess.WHITE):
+            costs[m] = abs(evaluator(board))
+        if(board.turn == chess.BLACK):
+            costs[m] = -abs(evaluator(board))
         board.pop()
-        print(m)
     return max(costs, key=costs.get), costs
 
+#print(getBetterMove(board))
+#print(getBetterMove(board_2))
+# it's working!!
