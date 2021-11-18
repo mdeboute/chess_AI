5115a4315668be1bb4f1fb0b73fc17b0cb8c8359
Baptiste-is <b.bedouret@gmail.com>
2021-11-18 13:19:23 +0100

    update

--- src/test.py
+++ src/test.py
@@ -1,14 +1,19 @@
 import time
 import chess
 from random import randint, choice
+import math
 
 
-board = chess.Board()
-board_1 = chess.Board("r1bqkbnr/pppp1ppp/8/8/3nP3/8/PPP2PPP/RNBQKB1R w KQkq - 0 5")
-board_2 = chess.Board("r1bqkb1r/ppppnpQp/8/8/4P3/8/PPP2PPP/RNB1KB1R b KQkq - 0 6")
+board = chess.Board(
+    "Q7/p3k3/7p/8/8/8/PPPP1PPP/RNBQKBNR b KQ - 0 15")
+board_2 = chess.Board(
+    "r1bqkb1r/ppppnpQp/8/8/4P3/8/PPP2PPP/RNB1KB1R b KQkq - 0 6")
+# r1bqkbnr/pppp1ppp/8/8/3nP3/8/PPP2PPP/RNBQKB1R w KQkq - 0 5
 
 # this function makes an exhaustive search of all the chess games by limiting the depth of
 # the search by a search parameter with the chess library and return the number of possibilities
+
+
 def exhaustiveSearch(board, depth):
     if depth == 0 or board.is_game_over():
         return 1
@@ -20,11 +25,15 @@ def exhaustiveSearch(board, depth):
             board.pop()
         return count
 
+
 # start = time.time()
 # print(exhaustiveSearch(board, 4))
 # end = time.time()
 # elapsed = end - start
 # print(f'Execution time: {elapsed:.2}ms')
+
+
+# HEURISTIQUE SHANNON VERSION 1
 
 def getPiece(board=board):
     white_piece = []
@@ -37,9 +46,18 @@ def getPiece(board=board):
             white_piece.append(symbol)
     return black_piece, white_piece
 
-# print(board.piece_map().items())
+
+print(board.piece_map().items())
+
 
 def shannonWeight(list):
+    if(board.is_game_over()):
+        return 0
+    if board.is_checkmate():
+        if (board.turn == chess.WHITE):
+            return -math.inf
+        if(board.turn == chess.BLACK):
+            return math.inf
     score = 0
     for piece in list:
         if(piece == 'k'):
@@ -68,12 +86,16 @@ def shannonWeight(list):
             score += 1
     return score
 
+
 def evaluator(board=board):
     black, white = getPiece(board)
     if(board.turn == chess.WHITE):
         return shannonWeight(white) - abs(shannonWeight(black))
     if(board.turn == chess.BLACK):
         return abs(shannonWeight(black)) - shannonWeight(white)
+
+
+# print(evaluator(board))
 
 def getBetterMove(board=board):
     costs = dict()
@@ -85,9 +107,112 @@ def getBetterMove(board=board):
         if(board.turn == chess.BLACK):
             costs[m] = -abs(evaluator(board))
         board.pop()
-    return max(costs, key=costs.get)
+    return max(costs, key=costs.get), costs
 
-# print(getBetterMove(board_1))
+
+# print(getBetterMove(board))
 # print(getBetterMove(board_2))
 # it's working!!
 
+
+# HEURISITQUE DE SHANNON VERSION 2
+
+def evaluate(board):
+    if(board.is_game_over()):
+        return 0
+    if board.is_checkmate():
+        if (board.turn == chess.WHITE):
+            return -math.inf
+        if(board.turn == chess.BLACK):
+            return math.inf
+    heuristic = 0
+    for n in board.piece_map():
+        piece = board.piece_map()[n]
+        score = 0
+        if piece.piece_type == 1:
+            score = 1
+        elif piece.piece_type == 2 or piece.piece_type == 3:
+            score = 3
+        elif piece.piece_type == 4:
+            score = 5
+        elif piece.piece_type == 5:
+            score = 9
+        elif piece.piece_type == 6:
+            score = 200
+        if piece.color:
+            heuristic += score
+        else:
+            heuristic -= score
+
+    return heuristic
+
+
+# print(board.piece_map()[2])
+print(evaluate(board))
+
+MAXDEPTH = 4
+
+
+def MiniMax(board, depth, maximizingPlayer):
+
+    if (depth == 0 or board.is_game_over()):
+        return evaluate(board)
+
+    if(maximizingPlayer):
+        maxEval = -math.inf
+        for move in board.generate_legal_moves():
+            board.push(move)
+            eval = MiniMax(board, depth-1, False)
+            # board.pop()
+            if(eval > maxEval):
+                maxEval = eval
+            board.pop()
+            return maxEval
+    else:
+        minEval = math.inf
+        for move in board.generate_legal_moves():
+            board.push(move)
+            eval = MiniMax(board, depth-1, True)
+            # board.pop()
+            if(eval < minEval):
+                minEval = eval
+            board.pop()
+        return minEval
+
+
+# print(MiniMax(board, 2, True))
+
+
+def alphaBeta(board, depth, alpha, beta, maximizingPlayer):
+    if (depth == 0 or board.is_game_over()):
+        return evaluator(board)
+    if(maximizingPlayer):
+        maxEval = -math.inf
+        for move in board.generate_legal_moves():
+            board.push(move)
+            eval = alphaBeta(board, depth-1, alpha, beta, False)
+            board.pop()
+            maxEval = max(maxEval, eval)
+            alpha = max(alpha, eval)
+            if(beta <= alpha):
+                break
+            return maxEval
+    else:
+        minEval = math.inf
+        for move in board.generate_legal_moves():
+            board.push(move)
+            eval = alphaBeta(board, depth-1, alpha, beta, True)
+            board.pop()
+            minEval = min(minEval, eval)
+            beta = min(beta, eval)
+            if(beta <= alpha):
+                break
+            return minEval
+
+
+# print(alphaBeta(board, 3, math.inf, -math.inf, True))
+
+# joueur aléatoire contre minimax niveau 3:
+
+
+# def match1(board):
